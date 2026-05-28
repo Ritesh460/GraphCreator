936fac103b927b0be3401623ce2ed29fffdfd997
Neptune990 <460219@bsd48.org>
2026-05-28 10:08:38 -0700

    add vertex and adding and remove edges

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 #include <iomanip>
 #include <climits>
 using namespace std;
+
 //variables for functions
 const int max = 20;
 const int label_size = 20;
+
 //graph struct
 struct Graph {
   char vertices[max][label_size];
   int adjacency[max][max];
   int vertexCount;
 }
+
 //initializes graph
 void initGraph(Graph &g) {
   g.vertexCount = 0;
@@ -19,6 +22,7 @@ void initGraph(Graph &g) {
     for (int j = 0; j < max; j++)
       g.adjacency[i][j] = 0;
 }
+
 //used for finding index of vertex
 int getIndex(Graph &g, char label[]) {
   for (int i = 0; i < g.vertexCount; i++) {
@@ -29,6 +33,52 @@ int getIndex(Graph &g, char label[]) {
   return -1;
 }
 
+//adding vertex
+void addVertex(Graph &g, char label[])
+{
+  if (g.vertexCount >= max)
+  {
+    cout << "Graph is full" << endl;
+    return;
+  }
+  if (getIndex(g, label) != -1)
+  {
+    cout << "Vertex already exists. Can't add :(" << endl;
+    return;
+  }
+  strcpy(g.vertices[g.vertexCount], label);
+  g.vertexCount++;
+  cout << "Vertex is added" << endl;
+}
+
+//adding edge
+void addEdge(Graph &g, char from[], char to[], int weight)
+{
+  int a = getIndex(g, from);
+  int b = getIndex(g, to);
+  if (a == -1 || b == -1)
+  {
+    cout << "This is invalid vertex" << endl;
+    return;
+  }
+  g.adjacency[a][b] = weight;
+  cout << "Edge added" << endl;
+}
+
+//removing edge
+void removeEdge(Graph &g, char from[], char to[])
+{
+  int a = getIndex(g, from);
+  int b = getIndex(g, to);
+  if (a == -1 || b == -1)
+  {
+    cout << "This is invalid vertex" << endl;
+    return;
+  }
+  g.adjacency[a][b] = 0;
+  cout << "Edge removed" << endl;
+}
+
 int main {
   Graph g;
   initGraph(g);
@@ -36,4 +86,10 @@ int main {
   char v1[label_size], v2[label_size];
   int weight;
   return 0;
+  cout << "1. add edge";
+  cout << "2. add vertex";
+  cout << "3. remove edge";
+  cout << "4. remove vertex";
+  cout << "5. find shortest path";
+  cout << "6. QUIT";
 }
